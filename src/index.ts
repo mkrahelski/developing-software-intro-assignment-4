aab27a18af7a81b93d59f96e90ac217605db9053
Mikita Krahelski <nikita.krahelski@gmail.com>
2020-10-01 16:33:40 -0700

    Added instanbul functionality, Updated Readme.md, Updated Version.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { expect } from "chai";
+/*import { expect } from "chai";
 import "mocha";
-import { SayHello } from "./hello";
 
 //https://journal.artfuldev.com/unit-testing-node-applications-with-typescript-using-mocha-and-chai-384ef05f32b2
 //for exmample of how to build tests
@@ -8,5 +7,14 @@ import { SayHello } from "./hello";
 console.log("Hello!");
 //only issue is I am not sure why having other files in src breaks things.
 
-SayHello();
 //specifically it was something odd that broke because functions can be imported. But maybe the test was meant to break unclear.
+
+import { hello } from "./hello";
+describe("Hello function", () => {
+  it("should return hello world", () => {
+    const result = hello();
+    expect(result).to.equal("Hello world!");
+  });
+});
+
+*/
